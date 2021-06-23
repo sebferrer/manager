38174399608470f7cbdbb62e9a51918867fc8ecd
mohammed-zahaf <mohammed.zahaf.ext@ovhcloud.com>
2021-06-23 14:40:09 +0200

    fix(project.notebooks): rename notebook class
    ref: MANAGER-6832
    
    Signed-off-by: mohammed-zahaf <mohammed.zahaf.ext@ovhcloud.com>

--- packages/manager/modules/pci/src/projects/project/notebooks/Notebook.class.js
+++ packages/manager/modules/pci/src/projects/project/notebooks/Notebook.class.js
@@ -10,7 +10,7 @@ import {
   NOTEBOOK_STORAGE_INFO,
   NOTEBOOK_TAGS,
   NOTEBOOK_VOLUME_TYPE,
-} from './Notebook.constants';
+} from './notebook.constants';
 
 const VOLUME_TYPE = 'volumeType';
 
@@ -51,6 +51,13 @@ export default class Notebook {
     this.spec.labels = Object.keys(this.labels).map((labelKey) =>
       Notebook.generateLabel(labelKey, this.labels[labelKey]),
     );
+
+    // Build a history actions
+    this.historyActions = [
+      { index: 1, action: 'create', date: this.createdAt },
+      { index: 2, action: 'restart', date: this.status.lastStartedAt },
+      { index: 3, action: 'stopped', date: this.status.lastStoppedAt },
+    ];
   }
 
   /**
@@ -195,14 +202,6 @@ export default class Notebook {
     return this.spec?.region;
   }
 
-  get cpu() {
-    return this.spec?.resources?.cpu;
-  }
-
-  get gpu() {
-    return this.spec?.resources?.gpu;
-  }
-
   get unsecureHttp() {
     return this.spec?.unsecureHttp;
   }
@@ -275,11 +274,11 @@ export default class Notebook {
   }
 
   get cpu() {
-    return this.spec.resources.cpu;
+    return this.spec?.resources?.cpu;
   }
 
   get gpu() {
-    return this.spec.resources?.gpu || 'none';
+    return this.spec?.resources?.gpu || 'none';
   }
 
   get memory() {
