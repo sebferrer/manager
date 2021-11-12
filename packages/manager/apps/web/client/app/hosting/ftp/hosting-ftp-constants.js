e44229b5fc95ac88fc604d3d61dedc883ba3a7a2
Lucas Chaigne <lucas.chaigne@ovhcloud.com>
2021-11-12 17:50:40 +0100

    feat(webhosting.ftp): add sftp information in ftp page (pro offer)
    ref: DTRSD-43496
    
    Signed-off-by: Lucas Chaigne <lucas.chaigne@ovhcloud.com>

---
+++ packages/manager/apps/web/client/app/hosting/ftp/hosting-ftp-constants.js
@@ -0,0 +1,9 @@
+export const SSH_STATE = {
+  ENABLED: 'active',
+  SFTP_ONLY: 'sftponly',
+  DISABLED: 'none',
+};
+
+export default {
+  SSH_STATE,
+};
--- packages/manager/apps/web/client/app/hosting/ftp/hosting-ftp.controller.js
+++ packages/manager/apps/web/client/app/hosting/ftp/hosting-ftp.controller.js
@@ -4,6 +4,7 @@ import get from 'lodash/get';
 import indexOf from 'lodash/indexOf';
 import isEmpty from 'lodash/isEmpty';
 import omit from 'lodash/omit';
+import { SSH_STATE } from './hosting-ftp-constants';
 
 angular.module('App').controller(
   'HostingTabFTPCtrl',
@@ -18,6 +19,7 @@ angular.module('App').controller(
       Alerter,
       Hosting,
       HostingUser,
+      WucUser,
     ) {
       this.$q = $q;
       this.$scope = $scope;
@@ -28,11 +30,13 @@ angular.module('App').controller(
       this.Alerter = Alerter;
       this.Hosting = Hosting;
       this.HostingUser = HostingUser;
+      this.WucUser = WucUser;
     }
 
     $onInit() {
       this.atInternet.trackPage({ name: 'web::hosting::ftp' });
 
+      this.guide = null;
       this.primaryUser = null;
       this.allowUpdateState = true;
       this.displayFtpExplorer = true;
@@ -51,6 +55,10 @@ angular.module('App').controller(
       this.search = {
         value: null,
       };
+
+      this.$scope.SSH_STATE = SSH_STATE;
+
+      this.getGuides();
 
       this.$scope.loadFtpInformations = (count, offset) =>
         this.loadFtpInformations(count, offset);
@@ -101,6 +109,14 @@ angular.module('App').controller(
         .then(({ capabilities }) => {
           this.displayFtpExplorer = get(capabilities, 'filesBrowser', false);
         });
+    }
+
+    getGuides() {
+      return this.WucUser.getUrlOf('guides').then((guides) => {
+        if (guides && guides.hostingFtp) {
+          this.guide = guides.hostingFtp;
+        }
+      });
     }
 
     resetSearch() {
